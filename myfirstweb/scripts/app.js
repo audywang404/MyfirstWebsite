b9793fa580dea6f821ada910a7a0d5e07bc13e2f
audywang404 <77103174+audywang404@users.noreply.github.com>
2025-03-16 14:34:13 +0800

    Delete myfirstweb directory

--- myfirstweb/scripts/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("searchButton").addEventListener("click", () => {
-    const query = document.getElementById("searchBox").value.trim();
-    if (query) {
-        window.open(`https://www.baidu.com/s?wd=${encodeURIComponent(query)}`, "_blank");
-    } else {
-        alert("请输入搜索内容！");
-    }
-});
